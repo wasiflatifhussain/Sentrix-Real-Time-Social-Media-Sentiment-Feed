341e74ca87f697c93a48e93428bfd487eb013b96
wasiflatifhussain <wasiflatifhussain@Wasifs-MacBook.local>
2025-12-19 14:36:08 +0800

    (chore): Remove .next folder from Git tracking

--- frontend/.next/server/app/favicon.ico/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
-R.c("server/chunks/[root-of-the-server]__6d9b2dbe._.js")
-R.c("server/chunks/node_modules_1d96145c._.js")
-R.m("[project]/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)").exports
--- frontend/.next/static/chunks/app_(root)_page_tsx_d9d331df._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_223386db._.js",
-  "static/chunks/_9f619910._.js"
-],
-    source: "dynamic"
-});
