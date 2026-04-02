536d660893bebf594d6efedba19873a1b371e7df
wasiflatifhussain <wasiflatifhussain@Wasifs-MacBook.local>
2026-04-02 20:18:41 +0800

    (chore): fixed navbar for mobile display

--- frontend/components/NavItems.tsx
+++ frontend/components/NavItems.tsx
@@ -4,11 +4,7 @@ import { NAV_ITEMS } from "@/lib/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const NavItems = ({
-  initialStocks,
-}: {
-  initialStocks: StockWithWatchlistStatus[];
-}) => {
+const NavItems = () => {
   const pathname = usePathname();
 
   const isActive = (path: string) => {
@@ -18,7 +14,7 @@ const NavItems = ({
   };
 
   return (
-    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+    <ul className="nav-list">
       {NAV_ITEMS.map(({ href, label }) => {
         // Skip search since it's now in center of header
         if (href === "/search") return null;
